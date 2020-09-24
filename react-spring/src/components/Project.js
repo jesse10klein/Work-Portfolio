5af001566993a4964b1cc50af81c892ea0424902
Jesse Klein <jesse10klein@gmail.com>
2020-09-24 20:48:50 +1000

    fixing img src

--- react-spring/src/components/Project.js
+++ react-spring/src/components/Project.js
@@ -4,7 +4,7 @@ const Project = ({imageURL, title, description}) => {
 
   return (
     <div className="projectCard">
-      <img src={`/${imageURL}`} alt="not laoded"></img>
+      <img src={`./${imageURL}`} alt="not laoded"></img>
       <h1>{title}</h1>
       <p>{description}</p>
     </div>
