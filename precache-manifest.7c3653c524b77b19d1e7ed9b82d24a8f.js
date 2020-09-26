dc714729f448ecdf8d2bc9de362f14c158b3c754
Jesse Klein <jesse10klein@gmail.com>
2020-09-26 20:20:02 +1000

    Updates

---
+++ precache-manifest.7c3653c524b77b19d1e7ed9b82d24a8f.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6358955ef208f6d343495ee0c4edb6aa",
+    "url": "/Work-Portfolio/index.html"
+  },
+  {
+    "revision": "8a469d6de8c9e24afef1",
+    "url": "/Work-Portfolio/static/css/2.c1a89a62.chunk.css"
+  },
+  {
+    "revision": "9d4752c14e49379f5990",
+    "url": "/Work-Portfolio/static/css/main.d957ba70.chunk.css"
+  },
+  {
+    "revision": "8a469d6de8c9e24afef1",
+    "url": "/Work-Portfolio/static/js/2.5704371a.chunk.js"
+  },
+  {
+    "revision": "41ec811c1b7b57076fd115bb78668a07",
+    "url": "/Work-Portfolio/static/js/2.5704371a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d4752c14e49379f5990",
+    "url": "/Work-Portfolio/static/js/main.4dd9652e.chunk.js"
+  },
+  {
+    "revision": "ef8cab98cc57925130e2",
+    "url": "/Work-Portfolio/static/js/runtime-main.5af08aaf.js"
+  },
+  {
+    "revision": "ccb6dc29877026e3cac0357ab6609e15",
+    "url": "/Work-Portfolio/static/media/audio.ccb6dc29.PNG"
+  },
+  {
+    "revision": "a59d02eebe3f97048075f45d9e97d835",
+    "url": "/Work-Portfolio/static/media/code.a59d02ee.PNG"
+  },
+  {
+    "revision": "2c1a7560c88ea83e6b2593cd07af8ad8",
+    "url": "/Work-Portfolio/static/media/gmail.2c1a7560.png"
+  },
+  {
+    "revision": "94db7e92e51707073eea9d740f69e975",
+    "url": "/Work-Portfolio/static/media/linkedin.94db7e92.png"
+  },
+  {
+    "revision": "e94e26e4bcff72ddb176933a1731cb9b",
+    "url": "/Work-Portfolio/static/media/sorting.e94e26e4.PNG"
+  },
+  {
+    "revision": "ee59fa4d84ec7f77a27f874093e73383",
+    "url": "/Work-Portfolio/static/media/telephone.ee59fa4d.png"
+  },
+  {
+    "revision": "b9e3ed9737cd4bdebab8a7269eeef816",
+    "url": "/Work-Portfolio/static/media/video.b9e3ed97.PNG"
+  }
+]);
--- react-spring/src/components/index.js
+++
@@ -1,6 +0,0 @@
-import Home from "./Home";
-import Projects from "./Projects";
-import Contact from "./Contact";
-import Navbah from "./Navbah";
-import Card from "./Card";
-export { Home, Projects, Contact, Navbah, Card };
